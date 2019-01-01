7157e7cf1aafe11b64957a05fe5890dae1644ff4
Cameron Kato <ckato005@gmail.com>
2019-01-01 00:10:31 -1000

    redid

--- Roomies/Home.swift
+++ Roomies/Home.swift
@@ -204,6 +204,11 @@ class Home: UIViewController {
         nc.addObserver(self, selector: #selector(self.getValue(notification:)), name: Notification.Name(rawValue: "done"), object: nil)
     }
     
+    override func viewDidDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self)
+        NotificationCenter.default.removeObserver(self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
